fb54cfb3a09bc07c12d6ed3ebd6430bda8f9cbc7
Jackson Rios <juniorrios1231@gmail.com>
2023-01-16 12:31:53 -0300

    PropTypes

--- meu-projeto/src/components/Item/Item.jsx
+++ meu-projeto/src/components/Item/Item.jsx
@@ -1,10 +1,21 @@
-function Item({marca}){
+import PropTypes from "prop-types"
+
+function Item({marca, ano_lancamento}){
     return(
         <>
-            <li>{marca}</li>
-            <p>Teste</p>
+            <li>{marca} - {ano_lancamento}</li>
         </>
     )
+}
+
+Item.prototype = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number
+}
+
+Item.defaultProps = {
+    marca: "Faltou a marca",
+    ano_lancamento: 0
 }
 
 export default Item;
--- meu-projeto/src/components/List/List.jsx
+++ meu-projeto/src/components/List/List.jsx
@@ -5,9 +5,10 @@ function List(){
         <>
           <h1>Minha lista</h1>
           <ul>
-            <Item marca='Ferrari'/>
-            <Item marca='Fiat'/>
-            <Item marca='Honda'/>
+            <Item marca='Ferrari' ano_lancamento={1985}/>
+            <Item marca='Fiat' ano_lancamento={1964}/>
+            <Item marca='Honda' ano_lancamento={1979}/>
+            <Item />
           </ul>  
         </>
 
